9460a848d2b842fc7d9b12aa88882bae70377d15
Thomas Hartwig <thomas.hartwig@gmail.com>
2023-08-16 16:56:17 +0200

    error message

--- frontend/src/app/error-dialog/error-dialog.component.ts
+++ frontend/src/app/error-dialog/error-dialog.component.ts
@@ -1,6 +1,5 @@
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, ChangeDetectorRef } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
-import { ChangeDetectorRef } from '@angular/core';
 
 @Component({
   selector: 'app-error-dialog',
@@ -8,16 +7,9 @@ import { ChangeDetectorRef } from '@angular/core';
   styleUrls: ['./error-dialog.component.css']
 })
 export class ErrorDialogComponent {
-  public data: { errorMessage: string };
   public message: string;
 
-  ngOnInit(): void {
-    this.cdr.detectChanges();
-  }
-
-  constructor(@Inject(MAT_DIALOG_DATA) data: { errorMessage: string }, private cdr : ChangeDetectorRef) {
-    this.data = data;
+  constructor(@Inject(MAT_DIALOG_DATA) data: { errorMessage: string }, private cdr: ChangeDetectorRef) {
     this.message = data.errorMessage;
-    this.cdr.markForCheck();
   }
 }
--- frontend/src/app/error-handler.service.ts
+++ frontend/src/app/error-handler.service.ts
@@ -1,22 +1,25 @@
-import { Injectable, ErrorHandler } from '@angular/core';
-import { MatDialog } from '@angular/material/dialog';
-import { ErrorDialogComponent } from './error-dialog/error-dialog.component';
+import {ErrorHandler, Injectable, NgZone} from '@angular/core';
+import {MatDialog} from '@angular/material/dialog';
+import {ErrorDialogComponent} from './error-dialog/error-dialog.component';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class ErrorHandlerService implements ErrorHandler {
 
-  constructor(private dialog: MatDialog) {}
+    constructor(private dialog: MatDialog, private zone: NgZone) {
+    }
 
-  handleError(error: any): void {
-    this.dialog.open(ErrorDialogComponent, {
-      data: { errorMessage: error.message || 'An unknown error occurred!' },
-      panelClass: 'custom-dialog-container',
-        position: {
-          top: 'auto',
-          bottom: '0'
-        }
-    });
-  }
+    handleError(error: any): void {
+        this.zone.run(() =>
+            this.dialog.open(ErrorDialogComponent, {
+                data: {errorMessage: error.message || 'An unknown error occurred!'},
+                restoreFocus: true,
+                panelClass: 'custom-dialog-container',
+                position: {
+                    top: 'auto',
+                    bottom: '0'
+                }
+            }));
+    }
 }
